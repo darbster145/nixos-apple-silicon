ec320014f9d4de5ff4e92d9b31d1411f6a2d9194
darbster145 <swoopmaster22@proton.me>
2026-03-27 20:14:12 -0600

    added fairdust branch

--- apple-silicon-support/packages/overlay.nix
+++ apple-silicon-support/packages/overlay.nix
@@ -1,4 +1,5 @@
 final: prev: {
   linux-asahi = final.callPackage ./linux-asahi { };
+  linux-asahi-fairydust = final.callPackage ./linux-asahi-fairydust { };
   uboot-asahi = final.callPackage ./uboot-asahi { };
 }
